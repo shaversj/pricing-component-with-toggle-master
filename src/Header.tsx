dd3d1d5c8a56b9f3060c84141863fa7c6e3ac716
Josh Shavers <shaversj@gmail.com>
2024-05-13 10:31:41 -0500

    Update header with mobile layout

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,9 +2,9 @@ import Toggle from "./Toggle.tsx";
 
 export default function Header() {
   return (
-    <header>
-      <h1>My App</h1>
-      <div>
+    <header className={"pb-[60px] pt-[44px] font-Montserrat"}>
+      <h1 className={"text-center text-[32px] font-semibold text-neutral-grayish-blue"}>Our Pricing</h1>
+      <div className={"flex items-center justify-center gap-x-6 pt-[30px] text-[15px] font-semibold leading-[28px] text-neutral-light-grayish-blue"}>
         <h2>Annually</h2>
         <Toggle />
         <h2>Monthly</h2>
--- src/Toggle.tsx
+++ src/Toggle.tsx
@@ -1,16 +1,12 @@
-import { Switch } from '@headlessui/react'
-import { useState } from 'react'
+import { Switch } from "@headlessui/react";
+import { useState } from "react";
 
-export default function Toggle () {
-  const [enabled, setEnabled] = useState(false)
+export default function Toggle() {
+  const [enabled, setEnabled] = useState(false);
 
   return (
-    <Switch
-      checked={enabled}
-      onChange={setEnabled}
-      className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600"
-    >
-      <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
+    <Switch checked={enabled} onChange={setEnabled} className="group inline-flex h-[32px] w-[56px] items-center rounded-full bg-gradient-to-r from-[#A3A8F0] to-[#696FDD] transition">
+      <span className="size-6 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-7" />
     </Switch>
-  )
+  );
 }
